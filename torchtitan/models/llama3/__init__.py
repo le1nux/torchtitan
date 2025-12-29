8069b2e25046314276f5d181dec03ab18aa91595
Max Luebbering <2804731+le1nux@users.noreply.github.com>
2025-12-29 17:25:01 +0100

    added llama 8B flavor without depth init

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -48,6 +48,16 @@ llama3_args = {
         multiple_of=1024,
         rope_theta=500000,
     ),
+    "8B_no_depth_init": TransformerModelArgs(
+        dim=4096,
+        n_layers=32,
+        n_heads=32,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=1024,
+        rope_theta=500000,
+        depth_init=False,
+    ),
     "70B": TransformerModelArgs(
         dim=8192,
         n_layers=80,
